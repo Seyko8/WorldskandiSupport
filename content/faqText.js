3891c9392c0d299c3e0a53174cd2243991b494a3
Seyko8 <sekokerodo@icloud.com>
2025-08-06 01:08:20 +0200

    faqText.js aktualisieren

--- content/faqText.js
+++ content/faqText.js
@@ -14,13 +14,15 @@ const faqText = `
 📡 Du erhältst alle Beiträge aus der Hauptgruppe direkt in einem privaten Kanal.
 
 5️⃣ *Welche Gruppe öffnet?*  
-🕒 Wir haben keine festen „Öffnungszeiten“. Halte diese Gruppe im Blick – dort bekommst du vor jeder Öffnung eine Nachricht.
+🕒 Wir haben keine festen „Öffnungszeiten“.  
+📢 Halte [diese Gruppe im Blick](https://t.me/+pgbomQsLFZNlOGZi) – dort bekommst du vor jeder Öffnung eine Nachricht.
 
 6️⃣ *Welche Gruppen gibt es?*  
-📋 Eine Übersicht aller Gruppen findest du [hier].
+📋 Eine Übersicht aller Gruppen findest du [hier](https://t.me/WorldskandiNavi).
 
 7️⃣ *Wodurch kann ich gebannt werden?*  
-🚫 Das Regelwerk gilt für alle User und Admins. Für einen klaren Überblick bitte einen Admin nach dem Regelwerk fragen.
+🚫 Das Regelwerk gilt für alle User und Admins.  
+Für einen klaren Überblick bitte einen Admin nach dem Regelwerk fragen.
 `;
 
 module.exports = faqText;
