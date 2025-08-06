035edc964bc6d869bbf75b2b899af2c5643df69c
Seyko8 <sekokerodo@icloud.com>
2025-08-06 03:30:06 +0200

    supportHandler.js aktualisieren

--- handlers/supportHandler.js
+++ handlers/supportHandler.js
@@ -1,11 +1,13 @@
-const supportState = require('./supportState');
 const setupTicketFlow = require('./supportTicketFlow');
 const setupAdminActions = require('./supportAdminActions');
+const setupMenu = require('./menuHandler');
 
-function supportHandler(bot) {
-  // Alle Teile laden
+function registerSupport(bot) {
+  const supportState = require('./supportState');
+
+  setupMenu(bot);
   setupTicketFlow(bot, supportState);
-  setupAdminActions(bot, supportState);
+  setupAdminActions(bot);
 }
 
-module.exports = supportHandler;
+module.exports = { registerSupport };
