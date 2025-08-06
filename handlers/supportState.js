3fd7c52f7b26609038142bdd8a8a9c704df2083a
Seyko8 <sekokerodo@icloud.com>
2025-08-06 03:54:31 +0200

    supportState.js aktualisieren

--- handlers/supportState.js
+++ handlers/supportState.js
@@ -1,4 +1,10 @@
+// Speichert den Status vom User (z. B. Thema gewählt, Nachricht schreiben etc.)
 const supportState = {};
+
+// Speichert aktive Support-Tickets → userId : threadId
 const activeThreads = {};
 
-module.exports = { supportState, activeThreads };
+module.exports = {
+  supportState,
+  activeThreads
+};
